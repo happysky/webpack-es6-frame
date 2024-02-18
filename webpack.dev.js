84bdfb56a1efe93a223ebe567c912f194e1327b9
wuxupeng <wuxupeng@360.cn>
2024-02-18 17:43:56 +0800

    webpack版本升级

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,25 +8,20 @@ module.exports = webpackMerge(
         mode: 'development',
         devServer: {
             host: "0.0.0.0",
-            contentBase: path.join(__dirname, 'dist'),
             compress: true,
             port: process.env.port || 9000,
-            watchContentBase: true,
-            inline: true,
             compress: true,
             open: true, //是否自动打开浏览器
-            clientLogLevel: 'error',
             //noInfo: true,
             // https: {
             //     key: fs.readFileSync(path.resolve(__dirname, `ssl/privkey.pem`)),
             //     cert: fs.readFileSync(path.resolve(__dirname, `ssl/server.pem`))
             // },
-            disableHostCheck: true,
             historyApiFallback: {
                 disableDotRule: true
             },
             //noInfo:true,
-            proxy: {
+            proxy: [{
                 '/nwesqintegralpublic/*': {
                     target: 'http://fuwu.rsj.beijing.gov.cn/',
                     changeOrigin: true,
@@ -36,6 +31,6 @@ module.exports = webpackMerge(
                         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
                     }
                 }
-            }
+            }]
         }
     });
